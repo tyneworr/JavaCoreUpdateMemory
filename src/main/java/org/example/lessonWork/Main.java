789a1971db50009bd6bf89ff16a8d60540d0097f
ronib <tyneworr@gmail.com>
2025-09-23 13:55:30 +0300

    commit two

---
+++ src/main/java/org/example/lessonWork/Main.java
@@ -0,0 +1,12 @@
+package org.example.lessonWork;
+
+public class Main {
+    public static void main(String[] args) {
+
+        int x = 26;
+        int y = 25;
+        double sum = (double)x/y;
+        System.out.println(sum);
+
+    }
+}
